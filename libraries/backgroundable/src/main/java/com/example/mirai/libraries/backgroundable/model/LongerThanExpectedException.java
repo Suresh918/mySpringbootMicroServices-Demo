813964afed2bc59ff0d4330d4a7b1ace5928918c
Suresh Uputhula <suresh918.uputhula@gmail.com>
2021-08-04 16:06:48 +0200

    Delete libraries/backgroundable directory

--- libraries/backgroundable/src/main/java/com/example/mirai/libraries/backgroundable/model/LongerThanExpectedException.java
+++
@@ -1,6 +0,0 @@
-package com.example.mirai.libraries.backgroundable.model;
-
-public class LongerThanExpectedException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-}
-
